e4d5fbe27114ae224cc971f707f3886d278652c3
Viktor Studenyak <viktor.studenyak@ruhr-uni-bochum.de>
2017-03-22 23:07:18 +0100

    Changed templates/Redesign

--- challenge_manager.py
+++ challenge_manager.py
@@ -4,14 +4,13 @@ import tornado.ioloop
 import tornado.web
 import tornado.options
 import tornado.httpserver
-#database imports
-import pymongo
 import mongoengine
-# models import
+
 from models import Challenge, ChallengePoint
 
 from tornado.options import define, options
 define("port", default=8000, help="run on the given port", type=int)
+
 
 class Application(tornado.web.Application):
     
@@ -40,56 +39,61 @@ class IndexHandler(tornado.web.RequestHandler):
         
 class ChallengeListHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render('challenge_list.html',
-                    challenges = Challenge.objects)
-        
+        self.render('challenge_list.html', challenges=Challenge.objects)
+
+
 class ChallengeDetailHandler(tornado.web.RequestHandler):
     def get(self, url):
-        self.render('challenge_detail.html',
-                   challenge = Challenge.objects.get(url=url))
-        
+        self.render('challenge_detail.html', challenge=Challenge.objects.get(url=url))
+
+
 class EditHandler(tornado.web.RequestHandler):
     '''
     Handles both adding and editing of Challenge model.
     You can create new Challenge instance via form, but you need to create at least one ChallengePoint to send the form.
     '''
-    def get(self,url = None):
+    def get(self, url=None):
+
         '''
         If in the url you have url keyword, then it is the argument by get method and it will be only edited
         '''
+
         if url:
-            self.render('edit_challenge.html',
-                        challenge = Challenge.objects.get(url=url))
+            self.render('edit_challenge.html', challenge=Challenge.objects.get(url=url))
         else:
             self.render('add.html')
          
-    def post(self, url = None):
+    def post(self, url=None):
+
         '''
         If url, then model will be only edited.
         '''
+
         challenge = dict()
-        challenge_fields = ['header','url','date_start',
-                            'date_end']
+        challenge_fields = ['header', 'url', 'date_start', 'date_end']
         if url:
             challenge = Challenge.objects.get(url=url) #gets challenge object parameters to edit them
         for field in challenge_fields:
-            challenge[field]= self.get_argument(field, None)
+            challenge[field] = self.get_argument(field, None)
         if url:
             challenge.save()
         else:
             point = dict()
-            point_fields=['title','key','done','required_time']
+            point_fields=['title', 'key', 'done', 'required_time']
             for field in point_fields:
-                point[field]=self.get_argument(field, None)
+                point[field] = self.get_argument(field, None)
             challenge['points'] = [ChallengePoint(**point)] #you have to create at least one point entry to send the form
             Challenge(**challenge).save()#you call new Challenge instance giving it arguments taken from dictionary and saves it
         self.redirect('/challenges/')
-        
+
+
 class EditPointHandler(tornado.web.RequestHandler):
+
     '''
     Implements editting and adding of challenge points.
     If key is fetched by url, then point will be just edited
     '''
+
     def get(self, url, key = None):
         if key:
             self.render('edit_challenge_point.html',
